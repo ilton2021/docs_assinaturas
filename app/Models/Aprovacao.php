fb2281391f52a69d0a236a179caebfe84ffaa5c4
Ilton Lima <ilton_aml@yahoo.com.br>
2021-10-07 10:48:39 -0300

    first commit

---
+++ app/Models/Aprovacao.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aprovacao extends Model
+{
+    protected $table = 'aprovacao';
+
+    protected $fillable = [
+
+    ];
+}
